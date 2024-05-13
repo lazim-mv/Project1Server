12a118a8fc3dc623e7e9972e2a5137d97ebad83c
lazim mv <lazimlatheef@gmail.com>
2024-05-13 11:50:02 +0530

    v2.0.0 adminPageNew

--- AddProduct/products.js
+++ AddProduct/products.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const multer = require('multer');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' }); // Set the destination folder for uploads
+const upload = multer({ dest: "uploads/" }); // Set the destination folder for uploads
 
-const UsedBike = mongoose.model('UsedBikes', {
+const UsedBike = mongoose.model("UsedBikes", {
   title: String,
   description: String,
   year: Number,
   price: Number,
   condition: String,
-  availabilityStatus: Boolean,
+  starredProduct: Boolean,
   color: String,
-  image: String, 
+  image: String,
   category: String,
-  // Assuming the image is stored as a base64 string
-  // Add other fields as needed
+  purchaseDate: Date,
+  soldDate: Date,
+  listingAddedDate: Date,
+  customerName: String,
+  customerNumber: Number,
 });
 
 // POST route to save bike data with image upload
-router.post('/admin/api/bikes', upload.single('image'), async (req, res) => {
+router.post("/admin/api/bikes", upload.single("image"), async (req, res) => {
   console.log(req.body); // Log the request body to check if image data is present
   console.log(req.file); // Log the uploaded file to check if it's received
   try {
@@ -32,49 +34,63 @@ router.post('/admin/api/bikes', upload.single('image'), async (req, res) => {
       year: req.body.year,
       price: req.body.price,
       condition: req.body.condition,
-      availabilityStatus: req.body.availabilityStatus,
+      starredProduct: req.body.starredProduct,
       color: req.body.color,
       image: req.body.image,
+      purchaseDate: req.body.purchaseDate,
+      soldDate: req.body.soldDate,
+      listingAddedDate: new Date(),
+      customerName: req.body.customerName, 
+      customerNumber: req.body.customerNumber,
     });
 
     await newBike.save();
     res.json({ success: true });
   } catch (error) {
-    console.error('Error saving bike:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error saving bike:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // GET route to retrieve all bikes
-router.get('/admin/api/bikes', async (req, res) => {
+router.get("/admin/api/bikes", async (req, res) => {
   try {
     const bikes = await UsedBike.find();
     res.json(bikes);
   } catch (error) {
-    console.error('Error retrieving bikes:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error retrieving bikes:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // GET route to retrieve a specific bike by ID
-router.get('/admin/api/bikes/:id', async (req, res) => {
+router.get("/admin/api/bikes/:id", async (req, res) => {
   try {
     const bike = await UsedBike.findById(req.params.id);
     res.json(bike);
   } catch (error) {
-    console.error('Error retrieving bike by ID:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error retrieving bike by ID:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // PUT route to update a specific bike by ID
-router.put('/admin/api/bikes/:id', upload.single('image'), async (req, res) => {
+router.put("/admin/api/bikes/:id", upload.single("image"), async (req, res) => {
   try {
-    const { title, description, year, price, condition, availabilityStatus, color, category } = req.body;
-
-    // Convert the uploaded image to base64
-    // const imageBuffer = req.file ? req.file.buffer : null;
-    // const imageBase64 = imageBuffer ? imageBuffer.toString('base64') : null;
+    const {
+      title,
+      description,
+      year,
+      price,
+      condition,
+      starredProduct,
+      color,
+      category,
+      purchaseDate,
+      soldDate,
+      customerName,
+      customerNumber,
+    } = req.body;
 
     await UsedBike.findByIdAndUpdate(req.params.id, {
       title,
@@ -82,25 +98,29 @@ router.put('/admin/api/bikes/:id', upload.single('image'), async (req, res) => {
       year,
       price,
       condition,
-      availabilityStatus,
+      starredProduct,
       color,
       category,
+      purchaseDate,
+      soldDate,
+      customerName,
+      customerNumber,
     });
     res.json({ success: true });
   } catch (error) {
-    console.error('Error updating bike by ID:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error updating bike by ID:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // DELETE route to delete a specific bike by ID
-router.delete('/admin/api/bikes/:id', async (req, res) => {
+router.delete("/admin/api/bikes/:id", async (req, res) => {
   try {
     await UsedBike.findByIdAndDelete(req.params.id);
     res.json({ success: true });
   } catch (error) {
-    console.error('Error deleting bike by ID:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error deleting bike by ID:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
